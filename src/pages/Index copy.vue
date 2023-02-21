9bd244a12c14f9d3b060b7871398d831a1c365a5
Darya <daryabsb@gmail.com>
2023-02-21 16:26:52 +0300

    added some pages to the project

---
+++ src/pages/Index copy.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Calendar from "@/components/Home/Calendar.vue";
+</script>
+<template>
+  <q-layout>
+
+    <div class="flex items-center justify-center py-12 ">
+      <div class="max-w-7xl mx-auto py-2 pt-1 ">
+
+        <main class="mx-auto">
+          <!-- Replace with your content -->
+          <div class="flex px-4 py-2 sm:px-0">
+            <div class=" rounded-lg border-4 border-dashed border-gray-200 p-4">
+              <calendar />
+            </div>
+
+          </div>
+          <!-- /End replace -->
+        </main>
+      </div>
+    </div>
+
+  </q-layout>
+</template>
+
---
+++ ui/task-management-ui/README.md
@@ -0,0 +1,6 @@
+# Task Management UI
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/knyttneve/pen/vYEzXOR](https://codepen.io/knyttneve/pen/vYEzXOR).
+
+Design by El Mamoune
+https://dribbble.com/shots/9528840-Tasks-Management-Web-App
