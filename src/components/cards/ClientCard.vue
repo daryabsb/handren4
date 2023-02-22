d3daf086ef8a1ecb503d5057dc21d0b171b22648
Darya Ibrahim <daryabsb@gmail.com>
2023-02-22 08:01:48 +0300

    added client card

---
+++ src/components/cards/ClientCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="q-app" style="min-height: 100vh;">
+        <div class="q-pa-md row items-start q-gutter-md">
+            <q-card class="my-card">
+                <q-item>
+                    <q-item-section avatar>
+                        <q-avatar>
+                            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+                        </q-avatar>
+                    </q-item-section>
+                    <q-item-section>
+                        <q-item-label>Title</q-item-label>
+                        <q-item-label caption>Subhead</q-item-label>
+                    </q-item-section>
+                </q-item>
+            </q-card>
+        </div>
+    </div>
+</template>
