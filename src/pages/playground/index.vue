884d12a8746ffa63a702d085b7ccbba663e31fe4
Darya Ibrahim <daryabsb@gmail.com>
2023-02-27 07:28:41 +0300

    fixed header on playground

--- src/pages/playground/index.vue
+++ src/pages/playground/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="video-bg pt-20">
+  <div class="video-bg">
     <video width="320" height="240" autoplay loop muted>
       <source src="https://assets.codepen.io/3364143/7btrrd.mp4" type="video/mp4" />
       Your browser does not support the video tag.
